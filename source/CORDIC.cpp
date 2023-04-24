1c546f5e5165916fb354113b954ab700d2b870c7
axstepko <alexstepko1@gmail.com>
2023-04-24 14:59:54 -0400

    Developed functional non-optimized CORDIC

---
+++ source/CORDIC.cpp
@@ -0,0 +1,130 @@
+/**
+ * @file CORDIC.c
+ * @author Alex Stepko (axstepko.com)
+ * @brief Rectangular to polar CORDIC converter
+ * @version 0.1
+ * @date 2023-04-17
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "CORDIC.h"
+
+/**
+ * @brief Rectangular to polar coordinate converter
+ *
+ * @param cos Rectangular real component input (cos)
+ * @param sin Rectangular imaginary component input (sin)
+ * @param mag Output vector magnitude
+ * @param theta Output vector angle (radians)
+ */
+void cordic(FIXED_POINT cos, FIXED_POINT sin, FIXED_POINT *mag, FIXED_POINT *theta)
+{
+        FIXED_POINT currentCos = cos;
+        FIXED_POINT currentSin = sin;
+        FIXED_POINT thetaRotated = 0.0;
+        FIXED_POINT ninetyDeg = (FIXED_POINT)M_PI_2;
+        FIXED_POINT circleRads = (FIXED_POINT)M_2_PI;
+        FIXED_POINT CORDIC_SCALE_FACTOR = 0.60735;
+
+        FIXED_POINT swapTemp = 0.0; // Temporary value used for swapping coordinates to perform initial vector rotation
+
+        // Rotate the vector to within quadrant I or IV, based upon the sin (y) value of the system
+
+        if ((currentSin > 0) & (currentCos < 0)) // Y value is in quadrant II
+        {
+// Rotate the vector by -90deg:
+// This is accomplished by swapping X, Y, and negating the swapped Y.
+#ifdef DEBUG_MODE
+                printf("QUAD II SWAP\n");
+                printf("BEFORE SWAP: X = %f, Y = %f\n", (float)currentCos, (float)currentSin);
+#endif
+                swapTemp = -1 * currentCos; // Set temp to opposite of X coordinate
+                currentCos = currentSin;    // Set Y to X
+                currentSin = swapTemp;      // Set X to negated Y
+
+                thetaRotated = ninetyDeg;       
+
+#ifdef DEBUG_MODE
+                printf("AFTER SWAP: X = %f, Y = %f\n", (float)currentCos, (float)currentSin);
+#endif
+        }
+        else if ((currentSin < 0) & (currentCos < 0)) // Y value is in quadrant III
+        {
+// Rotate the vector by +90deg:
+// This is accomplished by swapping the X and Y, and negating the swapped X.
+#ifdef DEBUG_MODE
+                printf("QUAD IV SWAP\n");
+                printf("BEFORE SWAP: X = %f, Y = %f\n", (float)currentCos, (float)currentSin);
+#endif
+                swapTemp = -1 * currentSin; // Set temp to opposite of X coordinate
+                currentSin = currentCos;    // Set Y to X
+                currentCos = swapTemp;      // Set X to negated Y
+                thetaRotated = -1 * ninetyDeg;
+#ifdef DEBUG_MODE
+                printf("AFTER SWAP: X = %f, Y = %f\n", (float)currentCos, (float)currentSin);
+#endif
+        }
+#ifdef DEBUG_MODE
+        else
+        {
+                printf("No coordinate swap needed.\n");
+                thetaRotated = 0.0;
+        }
+#endif
+// ^At this point, the vector is in quadrant I or IV.
+
+// Rotate the vector back to 0deg to find the thetaRotated value
+ROTATOR:
+        for (int i = 0; i < NUM_ITERATIONS; i++)
+        {
+                // Compute a shift iteration for the system:
+                FIXED_POINT cosShift = currentCos >> i;
+                FIXED_POINT sinShift = currentSin >> i;
+                // Determine which direction to rotate the vector, and rotate accordingly:
+#ifdef DEBUG_MODE
+                printf("Prior to rotation: X = %f, Y = %f, theta = %f\n", (float)currentCos, (float)currentSin, (float)thetaRotated);
+#endif
+                if (currentSin > 0) // Quadrant I, rotate clockwise
+                {
+#ifdef DEBUG_MODE
+                        printf("Clockwise rotation\n");
+#endif
+                        // Perform a clockwise rotation down to the X-axis
+                        currentCos = currentCos + sinShift;
+                        currentSin = currentSin - cosShift;
+
+                        // Update the rotated theta
+                        thetaRotated = thetaRotated + cordicPhase[i];
+                }
+                else // if (currentSin < 0) // Quadrant IV, rotate counter-clockwise
+                {
+#ifdef DEBUG_MODE
+                        printf("Counter-clockwise rotation\n");
+#endif
+                        // Counter-clockwise rotation up to the X-axis
+                        currentCos = currentCos - sinShift;
+                        currentSin = currentSin + cosShift;
+
+                        // Update the rotated theta
+                        thetaRotated = thetaRotated - cordicPhase[i];
+                }
+#ifdef DEBUG_MODE
+                printf("End of iteration\n");
+#endif
+                // End of iterations, so value can be updated
+        }
+
+        // Assign output values
+        currentCos = currentCos * cordicGain[NUM_ITERATIONS - 1];
+        *mag = currentCos;
+        *theta = thetaRotated;
+#ifdef DEBUG_MODE
+        printf("Magnitude: %f\n", (float)currentCos);
+        printf("Angle: %f\n", (float)thetaRotated);
+#endif
+}
