75ca4cddcf876088405947c6b885237d369cae45
axstepko <alexstepko1@gmail.com>
2023-04-25 13:17:01 -0400

    Initial commit

---
+++ source/datatypes.h
@@ -0,0 +1,14 @@
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+
+
+#define FIXED_BITS_M 24
+#define FIXED_BITS_N 12
+
+typedef ap_fixed<FIXED_BITS_M, FIXED_BITS_N> FIXED_POINT; //!< AP-Fixed datype for consistent calculation
+// typedef float FIXED_POINT; //!< Placeholder datatype for vscode development ease.
+
+
+
+#endif
